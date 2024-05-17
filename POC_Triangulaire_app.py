f847c0552f54da3e4737f5d2d4a5c0131452c767
primeverdata <primeverservicedata@gmail.com>
2024-05-17 11:12:56 +0200

    version stable, modifier choix des agences dans les combinaison possible

--- POC_Triangulaire_app.py
+++ POC_Triangulaire_app.py
@@ -1,69 +1,92 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import streamlit as st
-
 st.set_page_config(layout="wide")
-# Charger les données
-data= pd.read_csv("TestData_v1.csv", sep=";", encoding="utf8", index_col=[0])
-data.rename(columns={"Agence": 'agence_1', "Nom Tiers Agence": 'agence_2'}, inplace=True)
-
-st.write("## Sélection de la date des soldes")
-mois_an_solde = st.selectbox("## Choisissez une date :", sorted(data['Date Solde'].unique(), reverse=True))
-# st.title(f"Date des soldes : {mois_an_solde}")
 
 
-# df_all = data[(data["Date Solde"] == mois_an_solde ) & (data['Solde'] != 0)]
-df_all = data[(data["Date Solde"] == mois_an_solde )]
-# st.write(df_all)
+# Charger les données TODO : Rajouter un filtre sur le type de palettes 
+df = pd.read_csv("data/Mouvements_Emballages_ST_Details_cleaned.csv", sep=";", encoding="utf8", index_col=[0])
 
-# Affichage des agences côte à côte
-col1, col2, col3 = st.columns(3)
 
-# 1- Afficher le df_all des commandes confirmées et demander à l'utilisateur de Choisir la première agence (A)
-df = df_all[df_all["Solde confirmé ?"] == 'Oui']
-with col1:
-    st.write("## Sélection de l'agence A")
-    selected_agence_a = st.selectbox("Choisissez l'agence A :", sorted(df['agence_1'].unique()))
 
-# 2.1- L'utilisateur doit choisir dans la colonne 2 de df_a une deuxième agence (B)
-with col2:
-    st.write("## Sélection de l'agence B")
-    df_a = df[df['agence_1'] == selected_agence_a]
-    selected_agence_b = st.selectbox("Choisissez l'agence B :", sorted(df_a['agence_2'].unique()))
+with st.sidebar:
+    st.header("Filtres")
+    # Choisir une région 
+    region = st.selectbox("## Choisissez une région :", sorted(df['Région'].unique()))
+    df_region = df[(df['Région']==region)]
+    # Choisir une date
+    mois_an_solde = st.selectbox("## Choisissez une date :", sorted(df['Date Solde'].unique(), reverse=True))
+    df_region_date = df_region[(df_region['Région']==region) & (df_region["Date Solde"] == mois_an_solde )]
+    # Choisir un support 
+    support = st.selectbox("## Choissiez un support : ", df_region_date['Support'].unique())
+    df_region_date_support = df_region_date[df_region_date["Support"] == support]
+    # Choisir une agence A
+    selected_agence_a = st.selectbox("Choisissez l'agence A :", df_region_date_support['agence_1'].unique())
+    df_a = df_region_date_support[df_region_date_support['agence_1'] == selected_agence_a]
+    # Choisir une agence B 
+    selected_agence_b = st.selectbox("Choisissez l'agence B :", df_a['agence_2'].unique())
+    #---------------------------------
+list_triangulaire = []
+for agence_a in df_region_date_support['agence_1'].unique(): 
+    for agence_b in df_region_date_support['agence_2'].unique():
+        if not df_region_date_support[(df_region_date_support['agence_1']== agence_a) & (df_region_date_support['agence_2'] == agence_b)].empty: 
+            for agence_c in df_region_date_support['agence_1'].unique():
+                if not df_region_date_support[(df_region_date_support['agence_1']== agence_b) & (df_region_date_support['agence_2'] == agence_c)].empty: 
+                    if not df_region_date_support[(df_region_date_support['agence_1']== agence_c) & (df_region_date_support['agence_2'] == agence_a)].empty: 
+                        list_triangulaire.append([agence_a, agence_b, agence_c])
+liste_combinaisons  =  list_triangulaire       
+combinaisons_uniques = set()
+# Liste pour stocker les combinaisons uniques
+combinaisons_sans_doublons = []
+# Parcourir la liste de listes
+for combinaison in liste_combinaisons:
+    # Convertir la combinaison en tuple pour la rendre hashable
+    combinaison_tuple = tuple(combinaison)
+    # Vérifier si la combinaison n'existe pas déjà dans l'ensemble des combinaisons uniques
+    if combinaison_tuple not in combinaisons_uniques:
+        # Ajouter la combinaison à la liste des combinaisons uniques et à l'ensemble
+        combinaisons_sans_doublons.append(combinaison)
+        combinaisons_uniques.add(combinaison_tuple)
+
+#Afficher les combinaisons uniques sans doublons
+
+st.write(combinaisons_uniques)
+
 
 # 2.2- Filtrer sur agence_b dans le df --> en sortie df_b
-df_b = df[(df['agence_1'] == selected_agence_b) & (df['agence_2'] != selected_agence_a)]
+df_b = df_region_date_support[(df_region_date_support['agence_1'] == selected_agence_b) & (df_region_date_support['agence_2'] != selected_agence_a)]
 
-# 3.1- L'utilisateur doit choisir dans la colonne 2 de df_b une troisième agence (c)
-with col3:
-    st.write("## Sélection de l'agence C")
-    selected_agence_c = st.selectbox("Choisissez l'agence C :", sorted(df_b['agence_2'].unique()))
+# Filter uniquement sur les agences avec un solde existant avec A 
 
-# Calcul des soldes entre les agences sélectionnées
-Solde_ab = df[(df["agence_1"] == selected_agence_a) & (df["agence_2"] == selected_agence_b)]
-Solde_bc = df[(df["agence_1"] == selected_agence_b) & (df['agence_2'] == selected_agence_c)]
-# Solde_ac = df[(df['agence_2'] == selected_agence_a) & (df['agence_1'] == selected_agence_c)]
-Solde_ac = df[(df['agence_1'] == selected_agence_c) & (df['agence_2'] == selected_agence_a)]
-
-st.write("## Soldes entre A, B et C")
-soldes = pd.concat([Solde_ab, Solde_bc, Solde_ac], ignore_index=True)
-st.write(soldes)
-
-if Solde_ac.empty :
-        st.write(f"## Pas de triangulaire possible")
+# 3.1- L'utilisateur doit choisir dans la colonne 2 de df_b une troisième agence (c)  uniquement les agences qui ont un solde avec A 
+list_agences_c = []
+for agence in df_b['agence_2'].unique(): 
+    if not df_region_date_support[(df_region_date_support['agence_1']== agence) & (df_region_date_support['agence_2'] == selected_agence_a)].empty: 
+        list_agences_c.append(agence)
+if len(list_agences_c) == 0:
+    st.write(f"## Pas de triangulaire possible")
             
 else : 
-    # Concaténation des soldes
-   
+    
+    st.write(f"## {len(list_agences_c)} triangulaire(s) possible(s)")
+    selected_agence_c = st.selectbox("Choisissez l'agence C :", sorted(list_agences_c))
 
+    # Calcul des soldes entre les agences sélectionnées
+    Solde_ab = df_region_date_support[(df_region_date_support["agence_1"] == selected_agence_a) & (df_region_date_support["agence_2"] == selected_agence_b)]
+    Solde_bc = df_region_date_support[(df_region_date_support["agence_1"] == selected_agence_b) & (df_region_date_support['agence_2'] == selected_agence_c)]
+    Solde_ac = df_region_date_support[(df_region_date_support['agence_1'] == selected_agence_c) & (df_region_date_support['agence_2'] == selected_agence_a)]
+
+    st.write("## Soldes entre A, B et C")
+    soldes = pd.concat([Solde_ab, Solde_bc, Solde_ac], ignore_index=True)
+    st.write(soldes)
+
+
+    # Concaténation des soldes
     # Duplication du dataframe et inversion des colonnes et des signes
     df2 = soldes.copy()
     df2['agence_1'], df2['agence_2'] = df2['agence_2'], df2['agence_1']
     df2['Solde'] *= -1
     df_final = pd.concat([soldes, df2], ignore_index=True)
-
-
-
     # Affichage des barres pour chaque agence avec Streamlit
     st.write("## Visualisation des soldes entre les agences sélectionnées")
 
@@ -106,3 +129,4 @@ else :
         ax.axis('off')
     plt.tight_layout()
     st.pyplot(fig)
+    
