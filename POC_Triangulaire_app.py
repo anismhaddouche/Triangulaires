59e21fe7dc497fd9fb7179a9f51ac1d1b5f6982c
primeverdata <primeverservicedata@gmail.com>
2024-05-28 16:09:06 +0200

    fichier source corrigée

--- POC_Triangulaire_app.py
+++ POC_Triangulaire_app.py
@@ -5,51 +5,43 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 
-#  Charger les donnée
+#  Charger les données
 df = pd.read_csv(
     "data/Mouvements_Emballages_ST_Details_cleaned.csv",
     sep=";",
     encoding="utf8",
     index_col=[0],
 )
-df.drop(["Région"], axis=1, inplace=True)
-#  Filtre sur la région la date et le support
+
+# Création de sidebar pour choisir type de support
 with st.sidebar:
-    st.header("Filtres")
-    # Choisir une date
-    mois_an_solde = st.selectbox(
-        "## Choisissez une date :", sorted(df["Date Solde"].unique(), reverse=True)
-    )
-    df_date = df[
-                 (df["Date Solde"] == mois_an_solde)
-    ]
-    # Choisir un support
+    st.write(f"## Date de solde :    {df['Date Solde'].loc[0]}")
     support = st.selectbox(
-        "## Choissiez un support : ", df_date["Support"].unique()
+        "## Choisissez un support : ", df["Support"].unique()
     )
-    df_date_support = df_date[df_date["Support"] == support]
-    df_date_support = df_date_support[
-        df_date_support["Solde"] != 0
+    df_support = df[df["Support"] == support]
+    df_support = df_support[
+        df_support["Solde"] != 0
     ]
-    df_date_support.drop_duplicates(inplace=True)
-df_date_support.drop(["Date Solde", "Support"], axis=1, inplace=True)
+    df_support.drop_duplicates(inplace=True)
+df_support.drop(["Date Solde", "Support"], axis=1, inplace=True)
 
 # Calcul de toutes les triangulaires possibles en fonction des filtres précédents
 list_triangulaire = []
-for agence_a in df_date_support["agence_1"].unique():
-    for agence_b in df_date_support["agence_2"].unique():
-        if not df_date_support[
-            (df_date_support["agence_1"] == agence_a)
-            & (df_date_support["agence_2"] == agence_b)
+for agence_a in df_support["agence_1"].unique():
+    for agence_b in df_support["agence_2"].unique():
+        if not df_support[
+            (df_support["agence_1"] == agence_a)
+            & (df_support["agence_2"] == agence_b)
         ].empty:
-            for agence_c in df_date_support["agence_1"].unique():
-                if not df_date_support[
-                    (df_date_support["agence_1"] == agence_b)
-                    & (df_date_support["agence_2"] == agence_c)
+            for agence_c in df_support["agence_1"].unique():
+                if not df_support[
+                    (df_support["agence_1"] == agence_b)
+                    & (df_support["agence_2"] == agence_c)
                 ].empty:
-                    if not df_date_support[
-                        (df_date_support["agence_1"] == agence_c)
-                        & (df_date_support["agence_2"] == agence_a)
+                    if not df_support[
+                        (df_support["agence_1"] == agence_c)
+                        & (df_support["agence_2"] == agence_a)
                     ].empty:
                         list_triangulaire.append([agence_a, agence_b, agence_c])
 
@@ -64,26 +56,27 @@ if len(unique_agencies_list) == 0:
     st.write("# Pas de triangulaire possible")
 else:
     # Afficher les combinaisons uniques sans doublons
-    if st.button('Afficher les agences concernées pas des triangulaires'):
+    st.write(f"Afficher les agences concernées par des triangulaires :") 
+    if st.button('Afficher'):
         st.write(unique_agencies_list)
     selected_agence_a = st.selectbox("Choisissez l'agence A :", unique_agencies_list)
-    df_a = df_date_support[
-        df_date_support["agence_1"] == selected_agence_a
+    df_a = df_support[
+        df_support["agence_1"] == selected_agence_a
     ]
     # Choisir une agence B
     selected_agence_b = st.selectbox(
         "Choisissez l'agence B :", df_a["agence_2"].dropna().unique()
     )
-    df_b = df_date_support[
-        (df_date_support["agence_1"] == selected_agence_b)
-        & (df_date_support["agence_2"] != selected_agence_a)
+    df_b = df_support[
+        (df_support["agence_1"] == selected_agence_b)
+        & (df_support["agence_2"] != selected_agence_a)
     ]
     # L'utilisateur doit choisir dans la colonne 2 de df_b une troisième agence C uniquement les agences qui ont un solde avec A
     list_agences_c = []
     for agence in df_b["agence_2"].unique():
-        if not df_date_support[
-            (df_date_support["agence_1"] == agence)
-            & (df_date_support["agence_2"] == selected_agence_a)
+        if not df_support[
+            (df_support["agence_1"] == agence)
+            & (df_support["agence_2"] == selected_agence_a)
         ].empty:
             list_agences_c.append(agence)
     if len(list_agences_c) == 0:
@@ -97,17 +90,17 @@ else:
         # unique_agencies_list.remove(selected_agence_b)
         # selected_agence_c = st.selectbox("Choisissez l'agence C :", unique_agencies_list)
         # Calcul des soldes entre les agences sélectionnées
-        Solde_ab = df_date_support[
-            (df_date_support["agence_1"] == selected_agence_a)
-            & (df_date_support["agence_2"] == selected_agence_b)
+        Solde_ab = df_support[
+            (df_support["agence_1"] == selected_agence_a)
+            & (df_support["agence_2"] == selected_agence_b)
         ]
-        Solde_bc = df_date_support[
-            (df_date_support["agence_1"] == selected_agence_b)
-            & (df_date_support["agence_2"] == selected_agence_c)
+        Solde_bc = df_support[
+            (df_support["agence_1"] == selected_agence_b)
+            & (df_support["agence_2"] == selected_agence_c)
         ]
-        Solde_ac = df_date_support[
-            (df_date_support["agence_1"] == selected_agence_c)
-            & (df_date_support["agence_2"] == selected_agence_a)
+        Solde_ac = df_support[
+            (df_support["agence_1"] == selected_agence_c)
+            & (df_support["agence_2"] == selected_agence_a)
         ]
         # st.write("### Soldes entre A, B et Cs")
         soldes = pd.concat([Solde_ab, Solde_bc, Solde_ac], ignore_index=True)
@@ -117,7 +110,7 @@ else:
         df2["Solde"] *= -1
         df_final = pd.concat([soldes, df2], ignore_index=True)
         # Affichage des barres pour chaque agence avec Streamlit
-        st.write("Visualisation des soldes (*)")
+        st.write("Visualisation des soldes (*) :")
         # Déterminer le nombre de colonnes pour la grille
         num_cols = 3
         # Déterminer le nombre total d'agences et le nombre de lignes nécessaires
@@ -163,6 +156,6 @@ else:
         st.pyplot(fig)
         st.write("""
                 (*) Comment lire ces graphiques :
-                * Vert (solde négatif) : Lorsque la colonne est en vert, cela signifie que l’agence en titre doit des palettes à l’agence en bas de la colonne.
-                * Rouge (solde positif) : Lorsque la colonne est en rouge, c’est l’inverse. Cela signifie que l’agence en titre doit récupérer des palettes de l’agence en bas.
+                * Rouge (solde positif)   : Lorsque la colonne est en rouge, cela signifie que l’agence en titre doit des palettes à l’agence en bas de la colonne.
+                * Vert (solde négatif) : Lorsque la colonne est en vert, c’est l’inverse. Cela signifie que l’agence en titre doit récupérer des palettes de l’agence en bas.
                 """)
